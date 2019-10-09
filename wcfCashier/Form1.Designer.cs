58484963eeb2f6ced7c6355e0248c57790898058
fahmiazhar24 <fahmiazhar24@gmail.com>
2019-10-09 14:13:00 +0700

    commit

--- wcfCashier/Form1.Designer.cs
+++ wcfCashier/Form1.Designer.cs
@@ -36,9 +36,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(94, 172);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.Size = new System.Drawing.Size(67, 17);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "CASHIER";
             // 
             // Form1
             // 
