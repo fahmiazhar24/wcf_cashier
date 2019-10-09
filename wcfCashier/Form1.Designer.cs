ee20438635065cef95875ad0a723ac83f6ddd4cd
fahmiazhar24 <fahmiazhar24@gmail.com>
2019-10-09 15:11:46 +0700

    okeee

--- wcfCashier/Form1.Designer.cs
+++ wcfCashier/Form1.Designer.cs
@@ -33,16 +33,21 @@
             this.label3 = new System.Windows.Forms.Label();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.label4 = new System.Windows.Forms.Label();
-            this.comboBox_pesanan = new System.Windows.Forms.ComboBox();
             this.label_ttlhrg = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.textBox_jmlPEsan = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btn_Save = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(141, 118);
+            this.label1.Location = new System.Drawing.Point(15, 66);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(52, 17);
             this.label1.TabIndex = 0;
@@ -51,7 +56,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(141, 161);
+            this.label2.Location = new System.Drawing.Point(15, 109);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(64, 17);
             this.label2.TabIndex = 1;
@@ -60,7 +65,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(141, 230);
+            this.label3.Location = new System.Drawing.Point(15, 178);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(83, 17);
             this.label3.TabIndex = 2;
@@ -68,7 +73,7 @@
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(144, 270);
+            this.dateTimePicker1.Location = new System.Drawing.Point(18, 218);
             this.dateTimePicker1.Name = "dateTimePicker1";
             this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
             this.dateTimePicker1.TabIndex = 3;
@@ -76,24 +81,16 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(396, 118);
+            this.label4.Location = new System.Drawing.Point(270, 66);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(79, 17);
             this.label4.TabIndex = 4;
             this.label4.Text = "label_nobill";
             // 
-            // comboBox_pesanan
-            // 
-            this.comboBox_pesanan.FormattingEnabled = true;
-            this.comboBox_pesanan.Location = new System.Drawing.Point(399, 152);
-            this.comboBox_pesanan.Name = "comboBox_pesanan";
-            this.comboBox_pesanan.Size = new System.Drawing.Size(121, 24);
-            this.comboBox_pesanan.TabIndex = 5;
-            // 
             // label_ttlhrg
             // 
             this.label_ttlhrg.AutoSize = true;
-            this.label_ttlhrg.Location = new System.Drawing.Point(396, 230);
+            this.label_ttlhrg.Location = new System.Drawing.Point(270, 178);
             this.label_ttlhrg.Name = "label_ttlhrg";
             this.label_ttlhrg.Size = new System.Drawing.Size(46, 17);
             this.label_ttlhrg.TabIndex = 6;
@@ -102,7 +99,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(144, 194);
+            this.label5.Location = new System.Drawing.Point(18, 142);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(113, 17);
             this.label5.TabIndex = 7;
@@ -110,29 +107,70 @@
             // 
             // textBox_jmlPEsan
             // 
-            this.textBox_jmlPEsan.Location = new System.Drawing.Point(399, 194);
+            this.textBox_jmlPEsan.Location = new System.Drawing.Point(273, 142);
             this.textBox_jmlPEsan.Name = "textBox_jmlPEsan";
-            this.textBox_jmlPEsan.Size = new System.Drawing.Size(100, 22);
+            this.textBox_jmlPEsan.Size = new System.Drawing.Size(150, 22);
             this.textBox_jmlPEsan.TabIndex = 8;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(452, 66);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(221, 205);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(270, 109);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(79, 17);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "label_nobill";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btn_Save);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox_jmlPEsan);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label_ttlhrg);
+            this.groupBox1.Controls.Add(this.dateTimePicker1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Location = new System.Drawing.Point(45, 44);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(707, 326);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "CASHIER";
+            // 
+            // btn_Save
+            // 
+            this.btn_Save.Location = new System.Drawing.Point(18, 281);
+            this.btn_Save.Name = "btn_Save";
+            this.btn_Save.Size = new System.Drawing.Size(94, 26);
+            this.btn_Save.TabIndex = 11;
+            this.btn_Save.Text = "PRINT";
+            this.btn_Save.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox_jmlPEsan);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label_ttlhrg);
-            this.Controls.Add(this.comboBox_pesanan);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.dateTimePicker1);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -143,10 +181,13 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.ComboBox comboBox_pesanan;
         private System.Windows.Forms.Label label_ttlhrg;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox textBox_jmlPEsan;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btn_Save;
     }
 }
 
